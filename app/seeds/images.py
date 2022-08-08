00491b92b0482525df56dc71938455a57e1d4732
David Chung <dvchung00@gmail.com>
2022-08-08 05:15:29 +0000

    More seeders for sspots and images

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -143,64 +143,264 @@ def seed_images():
       spotId=7,
       imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image35-MalibuCalifornia.png'
     )
-    # image36 = Image(
-    #   spotId=8,
+    image36 = Image(
+      spotId=8,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image36-BoulderColorado.png'
+    )
+    image37 = Image(
+      spotId=8,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image37-BoulderColorado.png'
+    )
+    image38 = Image(
+      spotId=8,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image38-BoulderColorado.png'
+    )
+    image39 = Image(
+      spotId=8,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image39-BoulderColorado.jpg'
+    )
+    image40 = Image(
+      spotId=8,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image40-BoulderColorado.png'
+    )
+    image41 = Image(
+      spotId=9,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image41-Belize.png'
+    )
+    image42 = Image(
+      spotId=9,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image42-Belize.png'
+    )
+    image43 = Image(
+      spotId=9,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image43-Belize.png'
+    )
+    image44 = Image(
+      spotId=9,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image44-Belize.png'
+    )
+    image45 = Image(
+      spotId=9,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image45-Belize.png'
+    )
+    image46 = Image(
+      spotId=10,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image46-BeverlyHillsCalifornia.png'
+    )
+    image47 = Image(
+      spotId=10,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image47-BeverlyHillsCalifornia.png'
+    )
+    image48 = Image(
+      spotId=10,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image48-BeverlyHillsCalifornia.png'
+    )
+    image49 = Image(
+      spotId=10,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image49-BeverlyHillsCalifornia.png'
+    )
+    image50 = Image(
+      spotId=10,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image50-BeverlyHillsCalifornia.png'
+    )
+    image51 = Image(
+      spotId=11,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image51-BigSkyMontana.png'
+    )
+    image52 = Image(
+      spotId=11,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image52-BigSkyMontana.png'
+    )
+    image53 = Image(
+      spotId=11,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image53-BigSkyMontana.png'
+    )
+    image54 = Image(
+      spotId=11,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image54-BigSkyMontana.png'
+    )
+    image55 = Image(
+      spotId=11,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image55-BigSkyMontana.png'
+    )
+    image56 = Image(
+      spotId=12,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image56-RockawayBeachOregon.png'
+    )
+    image57 = Image(
+      spotId=12,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image57-RockawayBeachOregon.png'
+    )
+    image58 = Image(
+      spotId=12,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image58-RockawayBeachOregon.png'
+    )
+    image59 = Image(
+      spotId=12,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image58-RockawayBeachOregon.png'
+    )
+    image60 = Image(
+      spotId=12,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image60-RockawayBeachOregon.png'
+    )
+    image61 = Image(
+      spotId=13,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image61-ParkCityUtah.png'
+    )
+    image62 = Image(
+      spotId=13,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image62-ParkCityUtah.png'
+    )
+    image63 = Image(
+      spotId=13,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image63-ParkCityUtah.png'
+    )
+    image64 = Image(
+      spotId=13,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image64-ParkCityUtah.png'
+    )
+    image65 = Image(
+      spotId=13,
+      imageUrl='https://sharedndbucket.s3.us-west-1.amazonaws.com/image65-ParkCityUtah.png'
+    )
+    image66 = Image(
+      spotId=14,
+      imageUrl=''
+    )
+    image67 = Image(
+      spotId=14,
+      imageUrl=''
+    )
+    image68 = Image(
+      spotId=14,
+      imageUrl=''
+    )
+    image69 = Image(
+      spotId=14,
+      imageUrl=''
+    )
+    image70 = Image(
+      spotId=14,
+      imageUrl=''
+    )
+    # image71 = Image(
+    #   spotId=15,
     #   imageUrl=''
     # )
-    # image37 = Image(
-    #   spotId=8,
+    # image72 = Image(
+    #   spotId=15,
     #   imageUrl=''
     # )
-    # image38 = Image(
-    #   spotId=8,
+    # image73 = Image(
+    #   spotId=15,
     #   imageUrl=''
     # )
-    # image39 = Image(
-    #   spotId=8,
+    # image74 = Image(
+    #   spotId=15,
     #   imageUrl=''
     # )
-    # image40 = Image(
-    #   spotId=8,
+    # image75 = Image(
+    #   spotId=15,
     #   imageUrl=''
     # )
-    # image41 = Image(
-    #   spotId=9,
+    # image76 = Image(
+    #   spotId=16,
     #   imageUrl=''
     # )
-    # image42 = Image(
-    #   spotId=9,
+    # image77 = Image(
+    #   spotId=16,
     #   imageUrl=''
     # )
-    # image43 = Image(
-    #   spotId=9,
+    # image78 = Image(
+    #   spotId=16,
     #   imageUrl=''
     # )
-    # image44 = Image(
-    #   spotId=9,
+    # image79 = Image(
+    #   spotId=16,
     #   imageUrl=''
     # )
-    # image45 = Image(
-    #   spotId=9,
+    # image80 = Image(
+    #   spotId=16,
     #   imageUrl=''
     # )
-    # image46 = Image(
-    #   spotId=10,
+    # image81 = Image(
+    #   spotId=17,
     #   imageUrl=''
     # )
-    # image47 = Image(
-    #   spotId=10,
+    # image82 = Image(
+    #   spotId=17,
     #   imageUrl=''
     # )
-    # image48 = Image(
-    #   spotId=10,
+    # image83 = Image(
+    #   spotId=17,
     #   imageUrl=''
     # )
-    # image49 = Image(
-    #   spotId=10,
+    # image84 = Image(
+    #   spotId=17,
     #   imageUrl=''
     # )
-    # image50 = Image(
-    #   spotId=10,
+    # image85 = Image(
+    #   spotId=17,
+    #   imageUrl=''
+    # )
+    # image86 = Image(
+    #   spotId=18,
+    #   imageUrl=''
+    # )
+    # image87 = Image(
+    #   spotId=18,
+    #   imageUrl=''
+    # )
+    # image88 = Image(
+    #   spotId=18,
+    #   imageUrl=''
+    # )
+    # image89 = Image(
+    #   spotId=18,
+    #   imageUrl=''
+    # )
+    # image90 = Image(
+    #   spotId=18,
+    #   imageUrl=''
+    # )
+    # image91 = Image(
+    #   spotId=19,
+    #   imageUrl=''
+    # )
+    # image92 = Image(
+    #   spotId=19,
+    #   imageUrl=''
+    # )
+    # image93 = Image(
+    #   spotId=19,
+    #   imageUrl=''
+    # )
+    # image94 = Image(
+    #   spotId=19,
+    #   imageUrl=''
+    # )
+    # image95 = Image(
+    #   spotId=19,
+    #   imageUrl=''
+    # )
+    # image96 = Image(
+    #   spotId=20,
+    #   imageUrl=''
+    # )
+    # image97 = Image(
+    #   spotId=20,
+    #   imageUrl=''
+    # )
+    # image98 = Image(
+    #   spotId=20,
+    #   imageUrl=''
+    # )
+    # image99 = Image(
+    #   spotId=20,
+    #   imageUrl=''
+    # )
+    # image100 = Image(
+    #   spotId=20,
     #   imageUrl=''
     # )
 
@@ -244,21 +444,71 @@ def seed_images():
     db.session.add(image33)
     db.session.add(image34)
     db.session.add(image35)
-    # db.session.add(image36)
-    # db.session.add(image37)
-    # db.session.add(image38)
-    # db.session.add(image39)
-    # db.session.add(image40)
-    # db.session.add(image41)
-    # db.session.add(image42)
-    # db.session.add(image43)
-    # db.session.add(image44)
-    # db.session.add(image45)
-    # db.session.add(image46)
-    # db.session.add(image47)
-    # db.session.add(image48)
-    # db.session.add(image49)
-    # db.session.add(image50)
+    db.session.add(image36)
+    db.session.add(image37)
+    db.session.add(image38)
+    db.session.add(image39)
+    db.session.add(image40)
+    db.session.add(image41)
+    db.session.add(image42)
+    db.session.add(image43)
+    db.session.add(image44)
+    db.session.add(image45)
+    db.session.add(image46)
+    db.session.add(image47)
+    db.session.add(image48)
+    db.session.add(image49)
+    db.session.add(image50)
+    db.session.add(image51)
+    db.session.add(image52)
+    db.session.add(image53)
+    db.session.add(image54)
+    db.session.add(image55)
+    db.session.add(image56)
+    db.session.add(image57)
+    db.session.add(image58)
+    db.session.add(image59)
+    db.session.add(image60)
+    db.session.add(image61)
+    db.session.add(image62)
+    db.session.add(image63)
+    db.session.add(image64)
+    db.session.add(image65)
+    db.session.add(image66)
+    db.session.add(image67)
+    db.session.add(image68)
+    db.session.add(image69)
+    db.session.add(image70)
+    # db.session.add(image71)
+    # db.session.add(image72)
+    # db.session.add(image73)
+    # db.session.add(image74)
+    # db.session.add(image75)
+    # db.session.add(image76)
+    # db.session.add(image77)
+    # db.session.add(image78)
+    # db.session.add(image79)
+    # db.session.add(image80)
+    # db.session.add(image81)
+    # db.session.add(image82)
+    # db.session.add(image83)
+    # db.session.add(image84)
+    # db.session.add(image85)
+    # db.session.add(image86)
+    # db.session.add(image87)
+    # db.session.add(image88)
+    # db.session.add(image89)
+    # db.session.add(image90)
+    # db.session.add(image91)
+    # db.session.add(image92)
+    # db.session.add(image93)
+    # db.session.add(image94)
+    # db.session.add(image95)
+    # db.session.add(image96)
+    # db.session.add(image97)
+    # db.session.add(image98)
+    # db.session.add(image99)
+    # db.session.add(image100)
 
 
     db.session.commit()
