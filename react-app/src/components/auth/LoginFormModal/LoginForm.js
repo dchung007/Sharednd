b3695751fe320617e06b4f5f6d5ae735ccc9407a
David Chung <dvchung00@gmail.com>
2022-08-10 17:15:24 +0000

    Finished hed some error validations for signup form

--- react-app/src/components/auth/LoginFormModal/LoginForm.js
+++ react-app/src/components/auth/LoginFormModal/LoginForm.js
@@ -11,7 +11,14 @@ const LoginForm = ({ hideModal }) => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
-  // console.log("Modal lifeeeeeeeee LOGINFORMMMM")
+  const demoEmail = 'demo@email.com';
+  const demoPassword = 'password';
+
+  const demoLogin = async (e) => {
+    await dispatch(login(demoEmail, demoPassword))
+  }
+
+
 
 
   const onLogin = async (e) => {
@@ -36,50 +43,55 @@ const LoginForm = ({ hideModal }) => {
   }
 
   return (
-    <form className='login-form' onSubmit={onLogin}>
-      <div className='form-title'>
-        <h1>
-          Welcome to Sharednd
-        </h1>
-        <h4>
-          Login now!
-        </h4>
-      </div>
-      <ul className='login-form-list'>
-        <div className='errors-list'>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
+    <div>
+      <form className='login-form' onSubmit={onLogin}>
+        <div className='form-title'>
+          <h1>
+            Welcome to Sharednd
+          </h1>
+          <h4>
+            Login now!
+          </h4>
         </div>
-        <li>
-          <label htmlFor='email'>Email<span className="required">*</span>
-            <input
-              className="field-login"
-              name='email'
-              type='text'
-              placeholder='Email'
-              value={email}
-              onChange={updateEmail}
-            />
-          </label>
-        </li>
-        <li>
-          <label htmlFor='password'>Password<span className="required">*</span>
-            <input
-              className="field-login"
-              name='password'
-              type='password'
-              placeholder='Password'
-              value={password}
-              onChange={updatePassword}
-            />
-          </label>
-        </li>
-        <li className='login-button'>
-          <button className='login-button' type='submit'>Login</button>
-        </li>
-      </ul>
-    </form>
+        <ul className='login-form-list'>
+          <div className='errors-list'>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <li>
+            <label htmlFor='email'>Email<span className="required">*</span>
+              <input
+                className="field-login"
+                name='email'
+                type='text'
+                placeholder='Email'
+                value={email}
+                onChange={updateEmail}
+              />
+            </label>
+          </li>
+          <li>
+            <label htmlFor='password'>Password<span className="required">*</span>
+              <input
+                className="field-login"
+                name='password'
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={updatePassword}
+              />
+            </label>
+          </li>
+          <li className='login-button'>
+            <button className='login-button' type='submit'>Login</button>
+          </li>
+        </ul>
+      </form>
+      <div>
+        <button className='demo-button-login' onClick={demoLogin}>Log in with Demo User</button>
+      </div>
+    </div>
   );
 };
 
