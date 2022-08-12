74ebd61b334c8c7ff7644abf398a698e9059b621
David Chung <dvchung00@gmail.com>
2022-08-12 06:26:06 +0000

    Finished create booking styling

--- react-app/src/components/EditBooking/index.js
+++ react-app/src/components/EditBooking/index.js
@@ -1,21 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Calendar from 'react-calendar';
 import { useParams } from "react-router-dom";
 import { differenceInCalendarDays, differenceInDays, format } from 'date-fns';
-import { useDispatch } from "react-redux";
-import { addBooking, editBooking } from "../../store/bookings";
+import { useDispatch, useSelector } from "react-redux";
+import { addBooking, editBooking, getSpotBookings } from "../../store/bookings";
 import 'react-calendar/dist/Calendar.css';
 import './EditBooking.css'
 
 const EditBooking = ({ booking, hideForm }) => {
   const dispatch = useDispatch();
   const [date, setDate] = useState(new Date());
+  const [errors, setErrors] = useState([]);
+  const spotBookings = useSelector(state => state.bookings);
   // console.log(date)
 
-
+  useEffect(() => {
+    dispatch(getSpotBookings(booking.spotId))
+  }, [dispatch])
 
   const isSameDay = (a, b) => {
     return differenceInCalendarDays(a, b) === 0;
+  }
+
+  const checkDateInRange = (startDate, endDate, inputDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const input = new Date(inputDate);
+
+    // console.log(startDate, endDate, inputDate)
+    // console.log(inputDate >= startDate && inputDate <= endDate)
+    // console.log(start, end, input)
+    // console.log(input >= start && input <= end)
+    if (input >= start && input <= end) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const tileDisabled = ({ activeStartDate, date, view }) => {
+    // const disabledDates = []
+    let boolDate = false;
+    Object.values(spotBookings).forEach(booking => {
+      // console.log(booking)
+      const validDate = checkDateInRange(booking.startDate, booking.endDate, date);
+      if (validDate) {
+        // disabledDates.push(date);
+        boolDate = true;
+      }
+    })
+    // console.log(disabledDates)
+    return date < new Date() - 1 || boolDate
   }
 
   const handleCancel = (e) => {
@@ -26,34 +61,40 @@ const EditBooking = ({ booking, hideForm }) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     console.log(date)
-    if (isSameDay(date[0], date[1]) || !Array.isArray(date)) {
-      alert("Please select an endDate!")
+
+    const frontErrors = [];
+    if (!date) {
+      frontErrors.push('No dates currently selected. Must select a date.')
+      setErrors(frontErrors)
+    } else if (isSameDay(date[0], date[1])) {
+      frontErrors.push('Can not select same day for both start date and end date.')
+      setErrors(frontErrors)
+    } else if (date.length === 1) {
+      frontErrors.push('Please select an end date.')
+      setErrors(frontErrors)
+    } else if (frontErrors.length) {
+      setErrors(frontErrors)
       return;
-    }
+    } else {
+      const finalPrice = differenceInDays(date[1], date[0]) * booking.spot.price;
 
-    // if (date.length > 2) {
-    //   alert("Please select a date")
-    //   return;
-    // }
+      console.log(finalPrice)
 
-    const finalPrice = differenceInDays(date[1], date[0]) * booking.spot.price;
+      // console.log(format(date[0], 'yyyy-MM-dd'));
+      // console.log(format(date[1], 'yyyy-MM-dd'));
 
-    console.log(finalPrice)
+      const editedBooking = {
+        id: booking.id,
+        spotId: booking.spotId,
+        startDate: format(date[0], 'yyyy-MM-dd'),
+        endDate: format(date[1], 'yyyy-MM-dd'),
+        price: finalPrice
+      }
 
-    // console.log(format(date[0], 'yyyy-MM-dd'));
-    // console.log(format(date[1], 'yyyy-MM-dd'));
-
-    const editedBooking = {
-      id: booking.id,
-      spotId: booking.spotId,
-      startDate: format(date[0], 'yyyy-MM-dd'),
-      endDate: format(date[1], 'yyyy-MM-dd'),
-      price: finalPrice
-    }
-
-    let updatedBooking = await dispatch(editBooking(editedBooking))
-    if (updatedBooking) {
-      console.log('edit was successful!')
+      let updatedBooking = await dispatch(editBooking(editedBooking))
+      if (updatedBooking) {
+        console.log('edit was successful!')
+      }
     }
 
   }
@@ -66,9 +107,10 @@ const EditBooking = ({ booking, hideForm }) => {
         <button className="edit-booking-button" type="button" onClick={handleCancel}>Cancel</button>
         <div>
           <Calendar
-            value={date}
+            defaultValue={[new Date(booking.startDate), new Date(booking.endDate)]}
             onChange={setDate}
             selectRange={true}
+            tileDisabled={tileDisabled}
           />
         </div>
       </form>
