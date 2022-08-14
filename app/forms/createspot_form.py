4d26b0429c2945e2ddc84edc89cce93f0df59bda
David Chung <dvchung00@gmail.com>
2022-08-14 23:25:20 +0000

    Fixed edit spot name error

--- app/forms/createspot_form.py
+++ app/forms/createspot_form.py
@@ -20,7 +20,7 @@ def price_positive(form, field):
 
 
 class CreateSpotForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired(message='Name is required.'), Length(max=150, message='Spot name must be 150 characters or less.')])
+    name = StringField('name', validators=[DataRequired(message='Name is required.'), Length(max=150, message='Name must be 150 characters or less.')])
     address = StringField('address', validators=[Length(max=250, message='Address must be 250 characters or less.')])
     city = StringField('city', validators=[DataRequired(message='City is required.'), Length(max=100, message='City must be 100 characters or less.')])
     state = StringField('state', validators=[Length(max=100, message='State must be 100 characters or less.')])
