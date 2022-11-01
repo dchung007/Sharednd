8a399e0666e423e924946ec07193700ab173eac0
David Chung <dvchung00@gmail.com>
2022-11-01 05:02:23 +0000

    More validation

--- app/forms/createreview_form.py
+++ app/forms/createreview_form.py
@@ -3,6 +3,6 @@ from wtforms import IntegerField, StringField
 from wtforms.validators import DataRequired, InputRequired, Length
 
 class CreateReviewForm(FlaskForm):
-    spotId = IntegerField('spotId', validators=[DataRequired()])
+    spotId = IntegerField('spotId',validators=[DataRequired()])
     rating = IntegerField('rating', validators=[InputRequired(message='Rating is required.')])
     review = StringField('review', validators=[InputRequired(message='Review is required.'), Length(max=1000, message='Review must be 1000 characters or less.')])
